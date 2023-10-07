cbdc97a245d427b786ae0fe48b76c18073ad3f0e
rendi12345678 <hardleberg@email.com>
2023-10-07 10:37:57 +0700

    Create My Portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import Navbar from './components/Navbar';
+import './styles/reset.css';
+import './styles/navbar.css';
+import './styles/body.css';
+import Hero from './components/Hero';
+import './styles/hero.css';
+import About from './components/About';
+import Skills from './components/Skills';
+import Project from './components/Project';
 import './App.css';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Skills/>
+      <Project/>
+      <Contact/>
     </div>
   );
 }
